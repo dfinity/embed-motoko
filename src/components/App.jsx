78ef3fc6093419dc7625dfc214548be684979e52
Ryan Vandersmith <ryan.vandersmith@dfinity.org>
2022-08-01 09:12:24 -0600

    Rename component: Wrapper -> LandingPage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactTooltip from 'react-tooltip';
 import isEmbedded from '../utils/isEmbedded';
 import Embed from './Embed';
-import Wrapper from './Wrapper';
+import LandingPage from './LandingPage';
 
 export default function App() {
   return (
     <>
       <ReactTooltip />
-      {isEmbedded() ? <Embed /> : <Wrapper />}
+      {isEmbedded() ? <Embed /> : <LandingPage />}
     </>
   );
 }
