20c00877decbe116e74ccd17a3c008502e714115
rvanasa <ryan.vandersmith@dfinity.org>
2022-10-28 17:50:10 -0600

    Use 'generate()' from kusanagi package

--- src/utils/transpileKusanagi.ts
+++ src/utils/transpileKusanagi.ts
@@ -1,23 +1,5 @@
 import './ensureNodeGlobal';
-import { parse } from 'kusanagi';
-
-const generate = (node) => {
-  const gen = (item) => generate(item);
-  if (typeof node === 'string') {
-    return node;
-  }
-  if (Array.isArray(node)) {
-    return node.map(gen).join('');
-  }
-  if (node === undefined || node === null) {
-    return '';
-  }
-  if (node.$loc) {
-    return generate(node.token);
-  }
-  console.log('Unknown Kusanagi output:', node);
-  return `<UNKNOWN ${JSON.stringify(node)} >`;
-};
+import { parse, generate } from 'kusanagi';
 
 export default function transpileKusanagi(input: string): string {
   const node = parse(input);
