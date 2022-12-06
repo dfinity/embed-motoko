9c983c3b7eb4ea3138b2e9887954c5808c514dee
Ryan Vandersmith <ryan.vandersmith@dfinity.org>
2022-12-06 13:12:19 -0700

    Misc. UX improvements (#56)
    * Reduce run step limit
    
    * Adjust mobile layout for Embedly
    
    * Improve UX when importing large modules

--- src/components/Embed.tsx
+++ src/components/Embed.tsx
@@ -11,8 +11,9 @@ import { getEmbedHeight, getOutputHeight } from '../utils/getEmbedHeight';
 import preprocessMotoko from '../utils/preprocessMotoko';
 import Button from './Button';
 import CodeEditor, { EDITOR_FONT_SIZE } from './CodeEditor';
+import isMobile from '../utils/isMobile';
 
-mo.setRunStepLimit(1_000_000);
+mo.setRunStepLimit(100_000);
 
 const defaultLanguage = 'motoko'; // TODO: refactor
 
@@ -27,12 +28,15 @@ export const getEmbedSnippet = (src) =>
 />
 `.trim();
 
-export default function Embed({ limitHeight }) {
+let runDebounce: ReturnType<typeof setTimeout>;
+
+export default function Embed() {
   const [inputCode, setInputCode] = useCodeState();
   const [changed] = useChangedState();
   const [message, setMessage] = useState('');
   const [autoRun, setAutoRun] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [_output, setOutput] = useState<typeof mo.run>({});
 
   const { code, attributes /* , lineCount */ } = useMemo(() => {
     return preprocessMotoko(inputCode || '');
@@ -84,27 +88,33 @@ export default function Embed({ limitHeight }) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [packageData]);
 
-  const output = useMemo(() => {
+  useEffect(() => {
     if (!autoRun) {
-      return {};
+      // return {};
+      return;
     }
-    try {
-      setMessage('');
-      const file = mo.file('mo');
-      file.write(code);
-      const { stdout, stderr } = file.run();
-      const unitValueString = '() : ()\n';
-      return {
-        stdout: stdout.endsWith(unitValueString)
-          ? stdout.slice(0, -unitValueString.length)
-          : stdout,
-        stderr,
-      };
-    } catch (err) {
-      console.error(err);
-      return { stderr: err.message || String(err) };
-    }
+    clearTimeout(runDebounce);
+    runDebounce = setTimeout(() => {
+      try {
+        setMessage('');
+        const file = mo.file('mo');
+        file.write(code);
+        const { stdout, stderr } = file.run();
+        const unitValueString = '() : ()\n';
+        setOutput({
+          stdout: stdout.endsWith(unitValueString)
+            ? stdout.slice(0, -unitValueString.length)
+            : stdout,
+          stderr,
+        });
+      } catch (err) {
+        console.error(err);
+        setOutput({ stderr: err.message || String(err) });
+      }
+    }, 100);
   }, [code, autoRun]);
+
+  const output = autoRun ? _output : {};
 
   const handleCopy = useCallback(
     (fn: (link: string) => string) => {
@@ -170,8 +180,12 @@ export default function Embed({ limitHeight }) {
       // style={{ maxHeight: !!limitHeight && getEmbedHeight(lineCount) }}
     >
       <div
-        className="overflow-auto"
-        style={{ height: `calc(100% - ${outputHeight}px)` }}
+        // className="overflow-y-auto"
+        style={
+          isMobile()
+            ? {}
+            : { height: `calc(100% - ${outputHeight}px)`, overflowY: 'auto' }
+        }
       >
         <CodeEditor value={inputCode} onChange={handleChange} />
       </div>
