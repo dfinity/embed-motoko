2e452b57d7164dfa07b537fe1ec7d46af6b9a2a0
Ryan Vandersmith <ryan.vandersmith@dfinity.org>
2022-12-12 22:19:43 -0700

    Fix buttons overlapping with scrollbar (#64)
    * Fix responsive button padding
    
    * Fix buttons overlapping with scrollbar

--- src/components/Embed.tsx
+++ src/components/Embed.tsx
@@ -190,8 +190,8 @@ export default function Embed() {
       </div>
       <div
         className={classNames(
-          'space-y-2 p-2 sm:pr-4 absolute right-0 bottom-[100px] text-sm top-0 pointer-events-none [&>*]:pointer-events-auto z-10',
-          // isEmbedded() && 'hidden sm:block',
+          'space-y-2 p-2 absolute right-0 bottom-[100px] text-sm top-0 pointer-events-none [&>*]:pointer-events-auto z-10',
+          isEmbedded() && !isMobile() && 'sm:pr-6', // Fix buttons overlapping with scrollbar
         )}
       >
         {showEditButton ? (
