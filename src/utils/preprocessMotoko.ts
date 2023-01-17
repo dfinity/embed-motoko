7716b55916236303c120ed7e875850e6688a88e9
rvanasa <ryan.vandersmith@dfinity.org>
2023-01-17 09:02:58 -0700

    Re-enable custom packages

--- src/utils/preprocessMotoko.ts
+++ src/utils/preprocessMotoko.ts
@@ -1,6 +1,6 @@
 const TAB = '  ';
-// const LINE_COMMENT = '//';
-// const ANNOTATION = '@';
+const LINE_COMMENT = '//';
+const ANNOTATION = '@';
 
 // const prelude =
 //   `
@@ -26,70 +26,69 @@ export default function preprocessMotoko(code: string): PreprocessResult {
 
   const attributes: Attribute[] = [];
 
-  let lineCount = 1;
-  for (let i = 0; i < code.length; i++) {
-    if (code.charAt(i) === '\n') {
-      lineCount++;
-    }
-  }
+  // let lineCount = 1;
+  // for (let i = 0; i < code.length; i++) {
+  //   if (code.charAt(i) === '\n') {
+  //     lineCount++;
+  //   }
+  // }
 
-  // // Infer semicolons
-  // let nextIndent = 0;
-  // const reversedLines = code.split('\n').reverse();
-  // const lineCount = reversedLines.length;
-  // code = reversedLines
-  //   .map((line: string, i: number) => {
-  //     const trimmedLine = line.trim();
-  //     if (!trimmedLine) {
-  //       return line;
-  //     }
-  //     if (trimmedLine.startsWith(LINE_COMMENT)) {
-  //       const comment = trimmedLine.substring(LINE_COMMENT.length).trim();
-  //       if (comment.startsWith(ANNOTATION)) {
-  //         const [key, ...valueParts] = comment
-  //           .substring(ANNOTATION.length)
-  //           .trim()
-  //           .split(' ')
-  //           .map((s) => s.trim());
-  //         if (key) {
-  //           attributes.push({
-  //             key,
-  //             value: valueParts.join(' ').trim() || undefined,
-  //           });
-  //         }
-  //       }
-  //       return line;
-  //     }
+  let nextIndent = 0;
+  const reversedLines = code.split('\n').reverse();
+  const lineCount = reversedLines.length;
+  /* code = */ reversedLines
+    .map((line: string, i: number) => {
+      const trimmedLine = line.trim();
+      if (!trimmedLine) {
+        return line;
+      }
+      if (trimmedLine.startsWith(LINE_COMMENT)) {
+        const comment = trimmedLine.substring(LINE_COMMENT.length).trim();
+        if (comment.startsWith(ANNOTATION)) {
+          const [key, ...valueParts] = comment
+            .substring(ANNOTATION.length)
+            .trim()
+            .split(' ')
+            .map((s) => s.trim());
+          if (key) {
+            attributes.push({
+              key,
+              value: valueParts.join(' ').trim() || undefined,
+            });
+          }
+        }
+        return line;
+      }
 
-  //     let indent = 0;
-  //     while (indent < line.length && line.charAt(indent) === ' ') {
-  //       indent++;
-  //     }
+      let indent = 0;
+      while (indent < line.length && line.charAt(indent) === ' ') {
+        indent++;
+      }
 
-  //     const nextTrimmedLine = (reversedLines[i - 1] || '').trim();
-  //     // const previousTrimmedLine = (lines[i + 1] || '').trim();
+      const nextTrimmedLine = (reversedLines[i - 1] || '').trim();
+      // const previousTrimmedLine = (lines[i + 1] || '').trim();
 
-  //     if (
-  //       indent === nextIndent &&
-  //       !(
-  //         trimmedLine.endsWith(';') ||
-  //         trimmedLine.endsWith(',') ||
-  //         trimmedLine.endsWith('{') ||
-  //         trimmedLine.endsWith('(') ||
-  //         trimmedLine.endsWith('[') ||
-  //         ((trimmedLine.startsWith('/*') || !trimmedLine.includes('/*')) &&
-  //           trimmedLine.endsWith('*/')) ||
-  //         /^(else|catch)([^a-zA-Z0-9_]|$)/.test(nextTrimmedLine)
-  //       )
-  //     ) {
-  //       line += ';';
-  //     }
+      if (
+        indent === nextIndent &&
+        !(
+          trimmedLine.endsWith(';') ||
+          trimmedLine.endsWith(',') ||
+          trimmedLine.endsWith('{') ||
+          trimmedLine.endsWith('(') ||
+          trimmedLine.endsWith('[') ||
+          ((trimmedLine.startsWith('/*') || !trimmedLine.includes('/*')) &&
+            trimmedLine.endsWith('*/')) ||
+          /^(else|catch)([^a-zA-Z0-9_]|$)/.test(nextTrimmedLine)
+        )
+      ) {
+        line += ';';
+      }
 
-  //     nextIndent = indent;
-  //     return line;
-  //   })
-  //   .reverse()
-  //   .join('\n');
+      nextIndent = indent;
+      return line;
+    })
+    .reverse()
+    .join('\n');
 
   return {
     code,
