480231b0c297b734e879ffc24ec8fb68fd1d6acc
rvanasa <ryan.vandersmith@dfinity.org>
2023-11-28 12:34:25 -0700

    Adjust redirect logic

--- src/redirect.ts
+++ src/redirect.ts
@@ -2,10 +2,10 @@ import isEmbedded from './utils/isEmbedded';
 
 // Redirect to custom domain
 if (
-  (!isEmbedded &&
-    (window.location.hostname.endsWith('.icp0.io') ||
-      window.location.hostname.endsWith('.ic0.app'))) ||
-  window.location.hostname === 'embed.smartcontracts.org'
+  !isEmbedded &&
+  (window.location.hostname.endsWith('.icp0.io') ||
+    window.location.hostname.endsWith('.ic0.app') ||
+    window.location.hostname === 'embed.smartcontracts.org')
 ) {
   window.location.hostname = 'embed.motoko.org';
 }
